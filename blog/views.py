75308c4c04b08a4e4c046dd673f5049943721da8
mezzi <ezzi.mohammed1@yahoo.fr>
2023-06-09 05:44:27 +0000

    Finish database optimization

--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,14 @@ logger = logging.getLogger(__name__)
 
 
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
+
+
+def get_ip(request):
+  from django.http import HttpResponse
+  return HttpResponse(request.META['REMOTE_ADDR'])
 
 
 def post_detail(request, slug):
