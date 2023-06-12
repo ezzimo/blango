091b0704942e81b03d4f2824666288085cc56ce6
mezzi <ezzi.mohammed1@yahoo.fr>
2023-06-12 03:27:59 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -51,12 +51,17 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
         'debug_toolbar',
         'blango_auth',
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google',
     ]
 
     MIDDLEWARE = [
@@ -77,6 +82,16 @@ class Dev(Configuration):
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
     ACCOUNT_ACTIVATION_DAYS = 7
+
+    SITE_ID = 1
+
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+
+    ACCOUNT_EMAIL_REQUIRED = True
+
+    ACCOUNT_USERNAME_REQUIRED = False
+
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
     # controling the registration (set to False to close registration)
     REGISTRATION_OPEN = True
