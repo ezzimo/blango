f76b20d7eea63cc3a2b960efe17896b888b8872c
mohammed ezzi <ezzi.mohammed1@yahoo.fr>
2023-09-18 18:25:14 +0000

    Finish fetch and React hooks

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from django.utils import timezone
 from blog.forms import CommentForm
 from blog.models import Post
@@ -44,4 +45,6 @@ def post_detail(request, slug):
 
 
 def post_table(request):
-    return render(request, "blog/post-table.html")
+    return render(
+        request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
+    )
